8f780bc79132997ddc89dc14145150e566ce50bb
gvz42.web@gmail.com <gvz42.web@gmail.com>
2021-12-28 21:59:32 +0200

    Add number change logic

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -2,7 +2,8 @@
   <div>
     <input type="checkbox" />
     <span>{{ item.name }}</span>
-    <input type="color" v-model="color" @change="changeColor" />
+    <input type="number" v-model.number="number" @change="changeValue" />
+    <input type="color" v-model="color" @change="changeValue" />
   </div>
 </template>
 
@@ -12,16 +13,18 @@ export default {
   data() {
     return {
       color: this.item.color,
+      number: this.item.number,
     };
   },
   methods: {
-    changeColor() {
+    changeValue() {
       const payload = {
         listId: this.listId,
         itemId: this.item.id,
         newColor: this.color,
+        newNumber: this.number,
       };
-      this.$store.dispatch("changeColor", payload);
+      this.$store.dispatch("changeValue", payload);
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -8,31 +8,16 @@ export default new Vuex.Store({
     selected: [],
   },
   mutations: {
-    randomData(state) {
-      const nlists = Math.round(Math.random() * 10) + 3;
-      for (let i = 0; i < nlists; i++) {
-        const nitems = Math.round(Math.random() * 10) + 2;
-        state.lists.push({
-          id: i,
-          name: `List ${i + 1}`,
-          items: [],
-        });
-        for (let j = 0; j < nitems; j++) {
-          state.lists[i]["items"].push({
-            id: j,
-            name: `Item ${j + 1}`,
-            number: Math.round(Math.random() * 30) + 10,
-            color: "#" + Math.floor(Math.random() * 16777215).toString(16),
-          });
-        }
-      }
+    randomData(state, lists) {
+      state.lists = lists;
     },
-    changeColor(state, payload) {
+    changeValue(state, payload) {
       state.lists = state.lists.map((list) => {
         if (list.id === payload.listId) {
           list.items.map((item) => {
             if (item.id === payload.itemId) {
-              item.color = payload.newColor;
+              item.color = payload.newColor || item.color;
+              item.number = payload.newNumber || item.number;
             }
             return item;
           });
@@ -43,10 +28,28 @@ export default new Vuex.Store({
   },
   actions: {
     randomData({ commit }) {
-      commit("randomData");
+      const lists = [];
+      const nlists = Math.round(Math.random() * 10) + 3;
+      for (let i = 0; i < nlists; i++) {
+        const nitems = Math.round(Math.random() * 10) + 2;
+        lists.push({
+          id: i,
+          name: `List ${i + 1}`,
+          items: [],
+        });
+        for (let j = 0; j < nitems; j++) {
+          lists[i]["items"].push({
+            id: j,
+            name: `Item ${j + 1}`,
+            number: Math.round(Math.random() * 30) + 10,
+            color: "#" + Math.floor(Math.random() * 16777215).toString(16),
+          });
+        }
+      }
+      commit("randomData", lists);
     },
-    changeColor({ commit }, payload) {
-      commit("changeColor", payload);
+    changeValue({ commit }, payload) {
+      commit("changeValue", payload);
     },
   },
   getters: {

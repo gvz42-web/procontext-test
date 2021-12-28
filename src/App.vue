f11c01beced2f6541b6d8cb22fb4949f70aaa968
gvz42.web@gmail.com <gvz42.web@gmail.com>
2021-12-28 14:53:58 +0200

    Add random data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ lists }}
+    <Selector class="half" :lists="lists" />
+    <Display class="half" :lists="lists" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Selector from "@/components/Selector";
+import Display from "@/components/Display";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Display,
+    Selector,
+  },
+  data() {
+    return {
+      lists: {},
+    };
+  },
+  beforeMount() {
+    const nlists = Math.round(Math.random() * 10);
+    for (let i = 0; i < nlists; i++) {
+      const nitems = Math.round(Math.random() * 10);
+      this.lists[`List ${i + 1}`] = {};
+      for (let j = 0; j < nitems; j++) {
+        this.lists[`List ${i + 1}`][`Item ${j + 1}`] = {
+          number: Math.round(Math.random() * 50),
+          color: Math.floor(Math.random() * 16777215).toString(16),
+        };
+      }
+    }
   },
 };
 </script>
 
 <style>
 #app {
+  display: flex;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.half {
+  width: 50%;
+  height: 100%;
 }
 </style>

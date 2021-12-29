21fecc38c56d22acb803ea0e1817137ea7b01fb1
gvz42.web@gmail.com <gvz42.web@gmail.com>
2021-12-29 16:54:40 +0200

    Add display interface

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <div class="card">
-      <div v-for="list of lists" :key="list.id">
-        {{ list.name }}
-        <DrawItems
-          v-for="item of list.items"
-          :key="item"
-          :item="item"
-          :listId="list.id"
-        />
+      <div class="list" v-for="list of lists" :key="list.id">
+        <span class="name">{{ list.name }}</span>
+        <div class="items">
+          <DrawItems
+            v-for="item of list.items"
+            :key="item"
+            :item="item"
+            :listId="list.id"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -26,4 +28,17 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.list {
+  background-color: #fff;
+  width: 100%;
+  min-height: 40px;
+  margin-bottom: 20px;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 42px -11px rgba(34, 60, 80, 0.2);
+}
+.items {
+  padding: 20px;
+}
+</style>
